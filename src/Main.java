5aaefe4b68217bbe7c49c18ed834c5f3fe70094b
CesarAlfredo <97799656+CesarAlfredo@users.noreply.github.com>
2026-09-18 13:19:01 -0600

    Add Main class with application startup messages

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(
+            "Aplicacion desplegada correctamente"
+        );
+
+        System.out.println(
+            "Version: 1.0.0"
+        );
+    }
+}
